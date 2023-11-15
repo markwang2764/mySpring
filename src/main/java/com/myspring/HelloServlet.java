c42a3bd4d4cc8ae5201c71b7229ada2a87d81974
wangyuefeng <276410287@qq.com>
2023-11-15 17:15:04 +0800

    s

---
+++ src/main/java/com/myspring/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.myspring;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @program: mySpringFramework
+ * @description:
+ * @author: Mr.markWang 2764
+ * @create: 2023-11-15 17:11
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = "我的简易框架";
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
